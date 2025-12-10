d92dc437c4d71f7d55750375e5d14f02ee5b5d50
tkdlqjwmxkdl317-sudo <tkdlqjwmxkdl317@gmail.com>
2025-12-10 15:05:02 +0900

    Update ewm.R

--- ewm.R
+++ ewm.R
@@ -25,3 +25,39 @@ ewma_adjust <- function(x, alpha = NULL, span = NULL, adjust = FALSE) {
   }
   return(res)
 }
+
+ewmsd_adjust <- function(x, alpha = NULL, span = NULL, adjust = FALSE) {
+  
+  if (!is.null(alpha) && !is.null(span)) {
+    stop("alpha 와 span 중 하나만 입력해야 합니다.")
+  }
+  
+  if (is.null(alpha) && is.null(span)) {
+    stop("alpha 또는 span 중 하나를 반드시 입력해야 합니다.")
+  }
+  
+  if (!is.null(span)) {
+    alpha <- 2 / (span + 1)
+  }
+  
+  n <- length(x)
+  ewma_mean <- ewma_adjust(x, alpha = alpha, adjust = adjust)
+  ewma_var <- numeric(n)
+  ewma_sd <- numeric(n)
+  
+  if (!adjust) {
+    for (t in 2:n) {
+      diff_t <- x[t] - ewma_mean[t - 1]
+      ewma_var[t] <- alpha * diff_t^2 + (1 - alpha) * ewma_var[t - 1]
+      ewma_sd[t] <- sqrt(ewma_var[t])
+    }
+  } else {
+    for (t in 1:n) {
+      w <- (1 - alpha)^(t:1 - 1) # 가중치 벡터
+      centered_sq <- (x[1:t] - ewma_mean[t])^2
+      ewma_var[t] <- sum(w * centered_sq) / sum(w)
+      ewma_sd[t] <- sqrt(ewma_var[t])
+    }
+  }
+  return(ewma_sd)
+}
