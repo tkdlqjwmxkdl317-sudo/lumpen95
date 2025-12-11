7c527dd64fbdaddc48cef8512387ff7f7b6397d6
tkdlqjwmxkdl317-sudo <tkdlqjwmxkdl317@gmail.com>
2025-12-11 13:07:56 +0900

    Update ewm.R

--- ewm.R
+++ ewm.R
@@ -1,4 +1,4 @@
-ewma_adjust <- function(x, alpha = NULL, span = NULL, adjust = FALSE) {
+ewma <- function(x, alpha = NULL, span = NULL) {
   
   if (!is.null(alpha) && !is.null(span)) {
     stop("alpha 와 span 중 하나만 입력해야 합니다.")
@@ -11,53 +11,46 @@ ewma_adjust <- function(x, alpha = NULL, span = NULL, adjust = FALSE) {
   if (!is.null(span)) {
     alpha <- 2 / (span + 1)
   }
-  
-  if (!adjust) {
-    res <- accumulate(x, function(prev, xi) alpha * xi + (1 - alpha) * prev)
-  } else {
-    n <- length(x)
-    res <- numeric(n)
+
+  n <- length(x)
+  res <- numeric(n)
     
-    for (t in 1:n) {
-      w <- (1 - alpha)^(t:1 - 1) # 가중치 벡터
-      res[t] <- sum(w * x[1:t]) / sum(w)
-    }
+  for (t in 1:n) {
+    w <- (1 - alpha)^(t:1 - 1) # 가중치 벡터
+    res[t] <- sum(w * x[1:t]) / sum(w)
   }
   return(res)
 }
 
-ewmsd_adjust <- function(x, alpha = NULL, span = NULL, adjust = FALSE) {
+ewmsd <- function(x, alpha = NULL, span = NULL, bias = FALSE) {
   
   if (!is.null(alpha) && !is.null(span)) {
     stop("alpha 와 span 중 하나만 입력해야 합니다.")
   }
-  
   if (is.null(alpha) && is.null(span)) {
     stop("alpha 또는 span 중 하나를 반드시 입력해야 합니다.")
   }
-  
   if (!is.null(span)) {
     alpha <- 2 / (span + 1)
   }
   
   n <- length(x)
-  ewma_mean <- ewma_adjust(x, alpha = alpha, adjust = adjust)
-  ewma_var <- numeric(n)
-  ewma_sd <- numeric(n)
-  
-  if (!adjust) {
-    for (t in 2:n) {
-      diff_t <- x[t] - ewma_mean[t - 1]
-      ewma_var[t] <- alpha * diff_t^2 + (1 - alpha) * ewma_var[t - 1]
-      ewma_sd[t] <- sqrt(ewma_var[t])
+  ewma_mean <- ewma(x, alpha = alpha)
+  res <- numeric(n)
+
+  for (t in 1:n) {
+    w <- (1 - alpha)^(t:1 - 1) # 가중치 벡터
+    centered_sq <- (x[1:t] - ewma_mean[t])^2
+    
+    w_sum  <- sum(w)
+    w_sum2 <- sum(w^2)
+    
+    if (bias) {
+      denom <- w_sum
+    } else {
+      denom <- w_sum - w_sum2 / w_sum
     }
-  } else {
-    for (t in 1:n) {
-      w <- (1 - alpha)^(t:1 - 1) # 가중치 벡터
-      centered_sq <- (x[1:t] - ewma_mean[t])^2
-      ewma_var[t] <- sum(w * centered_sq) / sum(w)
-      ewma_sd[t] <- sqrt(ewma_var[t])
-    }
+    res[t] <- sqrt(sum(w * centered_sq) / denom)
   }
-  return(ewma_sd)
+  return(res)
 }
